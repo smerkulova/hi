271b463df56abcca963abb838290ec4724495214
smerkulova <121297003+smerkulova@users.noreply.github.com>
2026-03-26 14:56:42 +0000

    Udated HI.java

--- HI.java
+++ HI.java
@@ -1,32 +1,8 @@
-Last login: Thu Mar 19 11:52:28 on ttys002
-/Users/sofiatalaeva/.zshrc:6: command not found: ＃
-/Users/sofiatalaeva/.zshrc:9: command not found: ＃
-/Users/sofiatalaeva/.zshrc:40: unmatched '
-sofiatalaeva@MacBook-Air-Sofia ~ % open -a "TextEdit"
-sofiatalaeva@MacBook-Air-Sofia ~ % nano hello.txt
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  UW PICO 5.09                    File: hello.txt                     Modified  
+  
 
 class HI{ 
         public static void main(String[] args){ 
-                System.out.println("Hello World!")
+                System.out.println("Hello World today is a good day!")
 }
 }
 
@@ -34,15 +10,3 @@ class HI{
 
 
 
-
-
-
-
-
-
-
-
-
-
-^G Get Help  ^O WriteOut  ^R Read File ^Y Prev Pg   ^K Cut Text  ^C Cur Pos   
-^X Exit      ^J Justify   ^W Where is  ^V Next Pg   ^U UnCut Text^T To Spell  
